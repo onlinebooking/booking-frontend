8c78a144e9cba0eda0c5b2077d03d2181c8b4e07
Mauro Bianchi <bianchimro@gmail.com>
2016-02-23 22:06:54 +0100

    introduced react-bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,38 @@
 import React from 'react';
-
+import { Nav, Navbar, NavItem, MenuItem, NavDropdown} from 'react-bootstrap';
 
 export class NavBar extends React.Component {
 
     render(){
+
+        return (
+
+            <Navbar>
+            <Navbar.Header>
+              <Navbar.Brand>
+                <a href="#">OnlineBooking</a>
+              </Navbar.Brand>
+            </Navbar.Header>
+            {/*
+            <Nav>
+              <NavItem eventKey={1} href="#">Link</NavItem>
+              <NavItem eventKey={2} href="#">Link</NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            */}
+          </Navbar>
+        )
+
+
+
+
+
         return (<nav className="navbar navbar-inverse">
           <div className="container-fluid">
             <div className="navbar-header">
