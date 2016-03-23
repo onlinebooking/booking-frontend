3f1aaac37036831c5f61c44e570fd3c238169880
gffuma <fumagalli.gf@gmail.com>
2016-03-23 14:14:06 +0100

    HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,38 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'cheap-module-eval-source-map',
   entry: './src/main.js',
-  devtool: 'source-map',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/static/'
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         loader: 'babel',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        include: __dirname
       },
       //https://github.com/gowravshekar/bootstrap-webpack
-      { test: /\.css$/, loader: "style-loader!css-loader" },
-      { test: /\.scss$/, loader: "style-loader!css!sass" },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css!sass'
+      },
       {
         test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
         loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
-      },
+      }
     ]
   }
 };
