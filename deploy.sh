96613af2621176e76c85b4fae6b5492733ae4f72
Mauro Bianchi <bianchimro@gmail.com>
2016-06-29 10:46:03 +0200

    fixed deploy env var name£

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-$QANDO_DEPOLY_PATH/env/bin/ansible-playbook \
-$QANDO_DEPOLY_PATH/frontend_deploy.yml \
+$QANDO_DEPLOY_PATH/env/bin/ansible-playbook \
+$QANDO_DEPLOY_PATH/frontend_deploy.yml \
 --user root \
--i $QANDO_DEPOLY_PATH/hosts \
+-i $QANDO_DEPLOY_PATH/hosts \
 --extra-vars "local_deploy_dir=$(pwd)" \
 -vv
