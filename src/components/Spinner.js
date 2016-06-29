c24ef1b63d346442f5210071a89c0882b13d2fbf
gffuma <fumagalli.gf@gmail.com>
2016-06-29 15:53:11 +0200

    Easy Spinner for spin

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,12 +4,10 @@ import classNames from 'classnames';
 export default class Spinner extends React.Component {
 
   render() {
-    const spinnerClass = classNames('huge-loader', { 'loader-hidden' : !this.props.show })
+    //const spinnerClass = classNames('huge-loader', { 'loader-hidden' : !this.props.show })
     return (
-      <div className={spinnerClass}>
-        <p className="loading-spinner text-center">
-          
-        </p>
+      <div className="loading-spinner text-center">
+        <img src="/static/spinner.svg" />
       </div>
     );
   }
