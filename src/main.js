81ff53c89416dadeec829d796f95e9e78754fe4b
gffuma <fumagalli.gf@gmail.com>
2016-07-08 10:43:43 +0200

    Custom css option in iframe mode

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import 'bootswatch/yeti/bootstrap.css';
-import './scss/style.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { browserHistory } from 'react-router';
--- src/store/bootstrapStore.js
+++ src/store/bootstrapStore.js
@@ -2,6 +2,7 @@ import { omit } from 'lodash';
 import { replaceQuery } from '../actions/routing';
 import { loginWithToken, showModalLogin } from '../actions/auth';
 import { setIFrameMode } from '../actions/options';
+import { BOOKING_API_URL } from '../constants/Urls';
 
 // Login user with persistent local storage jwt token
 function loginUserWithLocalStorageToken(store) {
@@ -32,7 +33,7 @@ function showModalLoginFromQueryString(store) {
 
 function setIFrameModeFromQueryString(store) {
   const query = store.getState().routing.locationBeforeTransitions.query;
-  const { iframe, iframeWithHeader, iframeWithFooter } = query;
+  const { iframe, iframeWithHeader, iframeWithFooter, iframeCustomCss } = query;
 
   if (iframe) {
     store.dispatch(setIFrameMode({
@@ -40,9 +41,20 @@ function setIFrameModeFromQueryString(store) {
       footer: !!~~iframeWithFooter,
     }));
     store.dispatch(replaceQuery(omit(query, [
-      'iframe', 'iframeWithHeader', 'iframeWithFooter'])));
+      'iframe', 'iframeWithHeader', 'iframeWithFooter', 'iframeCustomCss'])));
     // For parent iframe resize
     require('iframe-resizer').iframeResizerContentWindow;
+  }
+
+  if (iframe && iframeCustomCss) {
+    // Append custom shop css
+    const style = document.createElement('link');
+    style.type = 'text/css';
+    style.href = BOOKING_API_URL + '/embed-css/' + iframe;
+    document.getElementsByTagName('head')[0].appendChild(style);
+  } else {
+    // Qando standar css
+    require('../scss/style.scss');
   }
 }
 
